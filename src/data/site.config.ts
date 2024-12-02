f46476a33f92e8e831c65988d69cb4ed6fc07054
F. Javier R. Donado <1581140+mrdonado@users.noreply.github.com>
2024-12-02 21:47:57 +0100

    feat: GTM integration and simple cookie banner

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -7,6 +7,7 @@ interface SiteConfig {
 	ogLocale: string
 	shareMessage: string
 	paginationSize: number
+	gtmId: string
 }
 
 export const siteConfig: SiteConfig = {
@@ -17,5 +18,6 @@ export const siteConfig: SiteConfig = {
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Sharing this with you:', // Message to share a post on social media
-	paginationSize: 9 // Number of posts per page
+	paginationSize: 9, // Number of posts per page,
+	gtmId: 'GTM-59KVPQ82' // Google Tag Manager ID
 }
