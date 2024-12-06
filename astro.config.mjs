79e17d62e4aa1bb8c1b22ba71a51644b6d1c300b
F. Javier R. Donado <1581140+mrdonado@users.noreply.github.com>
2024-12-06 14:17:15 +0100

    feat: open external links in new window

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,31 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import { siteConfig } from './src/data/site.config'
+import remarkRehype from 'remark-rehype'
+import { visit } from 'unist-util-visit'
 
 // https://astro.build/config
 export default defineConfig({
 	site: siteConfig.site,
 	markdown: {
-		remarkPlugins: [remarkReadingTime],
+		remarkPlugins: [
+			remarkReadingTime,
+			remarkRehype,
+		],
+		rehypePlugins: [
+      () => (tree) => {
+        // Walk through the tree to process `a` elements
+        visit(tree, 'element', (node) => {
+          if (node.tagName === 'a' && node.properties && node.properties.href) {
+            const isExternal = /^https?:\/\//.test(node.properties.href);
+            if (isExternal) {
+              node.properties.target = '_blank';
+              node.properties.rel = 'noopener noreferrer';
+            }
+          }
+        });
+      }
+    ],
 		drafts: true,
 		shikiConfig: {
 			theme: 'material-theme-palenight',
@@ -23,7 +42,7 @@ export default defineConfig({
 				experimentalThemes: {
 					light: 'vitesse-light',
 					dark: 'material-theme-palenight',
-				  },
+				},
 				wrap: true
 			},
 			drafts: true
