f75e2889b8806479bff7a6bf7c1e59e53ee1f2dc
F. Javier R. Donado <1581140+mrdonado@users.noreply.github.com>
2024-12-07 17:31:17 +0100

    feat: update the website description

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 	site: 'https://www.jdonado.com/', // Write here your website url
 	author: 'F. Javier R. Donado', // Site author
 	title: 'F. Javier R. Donado', // Site title.
-	description: 'Javier Donado - Web and mobile apps (iOS and Android). Photos and music too.', // Description to display in the meta tags
+	description: 'Javier Donado - Web and mobile apps (iOS and Android) - Photos - Music', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
 	shareMessage: 'Sharing this with you:', // Message to share a post on social media
