ce48713ec410a0df7fe71fd23607431e53922718
F. Javier R. Donado <1581140+mrdonado@users.noreply.github.com>
2025-06-21 10:54:57 +0200

    feat: new post - "Engineering the Economy"

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,8 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Focus & Tools', 'Writing Software', 'Projects', 'Music'] as const
+export const CATEGORIES = [
+	'Focus & Tools',
+	'Writing Software',
+	'Projects',
+	'Beyond Code',
+	'Music'
+] as const
