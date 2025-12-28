b91608ec7516224aafec312fbfd0e95b61a00eec
F. Javier R. Donado <1581140+mrdonado@users.noreply.github.com>
2025-12-28 21:20:41 +0200

    chore: upgrade tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, passthroughImageService } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import { remarkReadingTime } from './src/utils/readTime.ts'
 import { siteConfig } from './src/data/site.config'
 import remarkRehype from 'remark-rehype'
@@ -19,6 +19,7 @@ export default defineConfig({
 		inlineStylesheets: 'always',
 	},
 	vite: {
+		plugins: [tailwindcss()],
 		build: {
 			cssCodeSplit: true,
 			minify: 'esbuild',
@@ -63,7 +64,6 @@ export default defineConfig({
 			drafts: true
 		}),
 		sitemap(),
-		tailwind(),
 		astroImageTools
 	]
 })
